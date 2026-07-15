dceb4984cd11344cda246e6af671a5c30fe8e3cb
fatpeppapig <cezary@falba.net>
2026-07-15 17:10:05 +0200

    Better Navbar rendering on mobile.

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -13,17 +13,15 @@
 <header
     class="relative w-full bg-surface-brand px-4 py-1 flex items-center justify-between flex-wrap gap-y-2"
 >
-    <div class="flex w-1/3 md:hidden"></div>
-
-    <div class="flex md:w-1/3">
+    <div class="flex w-1/2 md:w-1/3">
         {@render brand?.()}
     </div>
 
-    <div class="flex md:hidden w-1/3 justify-end items-center gap-2">
+    <div class="flex md:hidden w-1/2 justify-end items-center gap-2">
         {@render actions?.()}
     </div>
 
-    <div class="flex w-full md:w-1/3 h-10 my-2">
+    <div class="flex w-full items-center justify-center md:w-1/3 h-10 my-2">
         {@render nav?.()}
     </div>
 
